0c72b06c63dcbb764eb0f3dab6a43a3498a572ed
Husain <husainkapadia2811@gmail.com>
2018-05-31 11:02:03 +0200

    Bartlett bug fixed

--- Bartlett_P.m
+++ Bartlett_P.m
@@ -1,7 +1,10 @@
 function P = Bartlett_P(Y, M)
     P = zeros(size(Y));
-    
+    m = mod(size(Y,2),M);
     for i = 0:M:(size(Y,2)-M)
         P(:,i+1:i+M) = repmat(sum(abs(Y(:,i+1:i+M)).^2, 2)/M, 1, M);
+        if(i==(size(Y,2)-M))
+            P(:,i+1:i+M-m) = repmat(sum(abs(Y(:,i+1:i+M-m)).^2, 2)/(M-m), 1, M-m);
+        end
     end
 end
