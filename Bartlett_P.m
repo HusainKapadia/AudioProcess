10f7b32c5974367f97adfcdc1caa054d3e57fff5
Husain <husainkapadia2811@gmail.com>
2018-06-26 21:06:45 +0200

    bartlett bug fix

--- Bartlett_P.m
+++ Bartlett_P.m
@@ -1,10 +1,14 @@
 function P = Bartlett_P(Y, M)
     P = zeros(size(Y));
     m = mod(size(Y,2),M);
-    
+
     for i = 0:M:size(Y,2)
-        if(i>=(size(Y,2)-M))
-            P(:,i+1:i+m) = repmat(sum(abs(Y(:,i+1:i+m)).^2, 2)/m, 1, m);
+        if(i>(size(Y,2)-M))
+            if(m == 0)
+                P(:,i) = sum(abs(Y(:,i)).^2, 2);
+            else 
+                P(:,i+1:i+m) = repmat(sum(abs(Y(:,i+1:i+m)).^2, 2)/m, 1, m);
+            end
         else
             P(:,i+1:i+M) = repmat(sum(abs(Y(:,i+1:i+M)).^2, 2)/M, 1, M);
         end
--- multimic.m
+++ multimic.m
@@ -10,7 +10,7 @@ n3 = genNoise(3, db, length(s));
 n4 = genNoise(4, 40, length(s));
 
 fs = 16000;                     %sampling frequency
-ind = 1:96240;                  %length of speech
+ind = 1:length(s);                  %length of speech
 m = 4;                          %number of mics
 l = 15;                         %frame length in ms
 o = 60;                         %percent overlap
@@ -18,11 +18,11 @@ o = 60;                         %percent overlap
 %% Generate Noisy Speech
 c = 340;                        %speed of sound (m/s)
 a = 40;                         %target direction angle (degrees)
-x = 0.1*(1:m-1);                       %distance between mics (m) 
-tau = x.*fs.*cos(pi*a/180)./c;     %Time delay (s)
+x = 0.1*(1:m-1);                %distance between mics (m) 
+tau = x.*fs.*cos(pi*a/180)./c;  %Time delay (s)
 
-s = repmat(s(ind), 1, m);          %clean speech
-n = [n1(ind) n2(ind) n3(ind) n4(ind)];             %noise
+s = repmat(s(ind), 1, m);                      %clean speech
+n = [n1(ind) n2(ind) n3(ind) n4(ind)];         %noise
     
 %%STFT with overlap
 S = stft(s, 4, l, o, m, fs);
@@ -61,45 +61,39 @@ end
 %%Single channel Wiener
 M = 8;
 Pse = Bartlett_P(S_e, M);
-[noise_psd1, S_o1] = mmse_noise_tracker(Pse, S_e);
-[noise_psd2, S_o2] = vad_noise_tracker(Pse, S_e);
-[noise_psd3, S_o3] = mmse_noise_tracker_spp(Pse, S_e);
-[noise_psd, S_o] = mmse_noise_tracker_spp(Pse,Y,'Wiener');
+%[noise_psd1, S_o1] = mmse_noise_tracker(Pse, S_e, 'SS');
+%[noise_psd2, S_o2] = vad_noise_tracker(Pse, S_e, 'Wiener');
+[noise_psd3, S_o3] = mmse_noise_tracker_spp(Pse, S_e, 'SS');
+%[noise_psd, S_o] = mmse_noise_tracker_spp(Pse, Y, 'Wiener');
 
 %%STIFT with overlap add
-s_o1 = stift(S_o1, 4, l, o, 1, fs);
-s_o2 = stift(S_o2, 4, l, o, 1, fs);
+s_e = stift(S_e, 4, l, o, 1, fs);
+%s_o1 = stift(S_o1, 4, l, o, 1, fs);
+%s_o2 = stift(S_o2, 4, l, o, 1, fs);
 s_o3 = stift(S_o3, 4, l, o, 1, fs);
 
 %% Plots
-t = (1:96240)/fs;
+t = (1:length(y))./fs;
 figure()
 subplot(3,1,1)
-plot(s_o1), title('Noisy Speech');
+plot(t, y), title('Noisy Speech');
+axis([0 length(t)/fs -0.7 0.7]);
 subplot(3,1,2)
-plot(s_o2), title('Corrected Speech');
+plot(t, s_e), title('MVDR output');
+axis([0 length(t)/fs -0.7 0.7]);
 subplot(3,1,3)
-plot(s_o3), title('Clean Speech');
-
+plot(t, s_o3), title('Estimated Clean Speech');
+axis([0 length(t)/fs -0.7 0.7]);
 %% Evaluation
-SSNR1 = ssnr(s(1:length(s_o),1), s_o1, fs)
-STOI1 = stoi(s(1:length(s_o),1), s_o1, fs)
-
-SSNR2 = ssnr(s(1:length(s_o),1), s_o2, fs)
-STOI2 = stoi(s(1:length(s_o),1), s_o2, fs)
-
-SSNR3 = ssnr(s(1:length(s_o),1), s_o3, fs)
-STOI3 = stoi(s(1:length(s_o),1), s_o3, fs)
-
-plot(t,y(ind)), title('Noisy Speech');
-axis([1 96240/fs -0.5 0.5]);
-
-subplot(3,1,2)
-plot(t,s_o(ind)), title('Corrected Speech');
-axis([1 96240/fs -0.5 0.5]);
-
-subplot(3,1,3)
-plot(t,s(ind)), title('Clean Speech');
-axis([1 96240/fs -0.5 0.5]);
+%SSNR1 = ssnr(s(1:length(s_o1),1), s_o1, fs)
+%STOI1 = stoi(s(1:length(s_o1),1), s_o1, fs)
+% 
+% SSNR2 = ssnr(s(1:length(s_o2),1), s_o2, fs)
+% STOI2 = stoi(s(1:length(s_o2),1), s_o2, fs)
+% 
+SSNR = ssnr(s(1:length(y),1), y(:,1), fs);
+SSNR3 = ssnr(s(1:length(s_o3),1), s_o3, fs);
+STOI3 = stoi(s(1:length(s_o3),1), s_o3, fs)
+imp = SSNR3 - SSNR
 %% Sound
 sound(s_o3, fs)
